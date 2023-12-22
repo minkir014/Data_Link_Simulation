b0b5ff74fd707ed371bdb5483d67d4a1de1a4b66
mtheggi <mtheggi@gmail.com>
2023-12-22 14:38:28 +0200

    coordinatoer first draft :+1:

--- src/Coordinator.cc
+++ src/Coordinator.cc
@@ -14,15 +14,57 @@
 // 
 
 #include "Coordinator.h"
-
+#include <string>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std ;
 Define_Module(Coordinator);
 
 void Coordinator::initialize()
 {
     // TODO - Generated method body
+
+    // assuming coordinator.txt contains a single line of the form: [1 5] 1->nodeId, 5->startTime
+    std::ifstream inputFile("coordinator.txt");
+    std::string line;
+
+    std::getline(inputFile, line);
+
+    cout << line << endl;
+
+
+    std::istringstream iss(line);
+    char dum;
+    int nodeId =0 ;
+    double startTime= 1; // random intialize
+
+    cout <<"coordinator reads : " <<  nodeId << " " << startTime << endl;
+
+
+    if (!(iss >> dum >> nodeId >> startTime))
+    {
+        cout << "Error parsing the line no msg is send " << endl;
+        EV << "Error parsing line: " << line << "\n";
+        return ;
+    }
+    // craete a message
+    cMessage *msg = new cMessage(std::to_string(nodeId).c_str());
+    // scheduale the message to the node
+    scheduleAt((simtime_t)startTime, msg);
+
 }
 
 void Coordinator::handleMessage(cMessage *msg)
 {
     // TODO - Generated method body
+    // send the message to the node
+    cMessage * start = new cMessage("start");
+    if(string(msg->getName())== "0"){
+        send(start, "out0");
+    }else {
+        send(start, "out1");
+    }
+
+
 }
